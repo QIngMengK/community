a095f0f0e3bd9def4ce6d770e58b4d7a9d3d4b7e
QIngMengK <157796080+QIngMengK@users.noreply.github.com>
2025-04-18 20:46:46 +0800

    修改配置文件

--- src/main/java/life/dream/community/controller/AuthorizeController.java
+++ src/main/java/life/dream/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.dream.community.dto.AccessTokenDTO;
 import life.dream.community.dto.GithubUser;
 import life.dream.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,18 +13,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("Ov23li2JcwA5O8sSuHnQ");
-        accessTokenDTO.setClient_secret("244cf6c97a6a1d47f1150391c65c97887dbeacff");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-       System.out.println(user.getName());
+        System.out.println(user.getName());
         return "index";
     }
 }
